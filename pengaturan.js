d8031ecf5b666373edc11736db86146e5a6ca94c
sudutkamera-project <sudutkamera.project96@gmail.com>
2026-09-09 11:26:54 +0700

    Update Google Drive API Key in pengaturan.js

--- pengaturan.js
+++ pengaturan.js
@@ -8,5 +8,5 @@ const CONFIG_APP = {
     namaVendor: "SUDUT KAMERA PROJECT ",
 
     // 2. Google Drive API Key (Penting agar galeri bisa memuat foto)
-    googleApiKey: "AIzaSyAZczbLeVTXl-QKqLDJFWQLCd-lW9jqqxo"
+    googleApiKey: "AIzaSyCsDgwn_3NcJtSZN2Zw0qTXlSIvOI2Q5Ac"
 };
